fb867929222f2032958e631e261332bafa43df3a
Peter Anderson <pete.anderson@gmail.com>
2019-04-06 15:48:42 +0200

    Few minor tweaks

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 var board = document.getElementById('board');
 var racers = document.getElementsByClassName('racer');
 var boostButton = document.getElementById('boost-button');
-var leftDriver1 = 0;
-var leftDriver2 = 0;
-var leftDriver3 = 0;
-var leftDriver4 = 0;
+var leftRacingCar = 0;
+var leftMotorbike = 0;
+var leftPoliceCar = 0;
+var leftTractor = 0;
 
 // get a random number between 0 and 20
-function getRandomNumber() {
-    return Math.floor(Math.random() * 21);
+function getRandomNumber(num) {
+    return Math.floor(Math.random() * num);
 }
 
 board.addEventListener("click", function() {
-    leftDriver1 += getRandomNumber();
-    leftDriver2 += getRandomNumber();
-    leftDriver3 += getRandomNumber();
-    leftDriver4 += getRandomNumber();
-    racers[0].style.left = leftDriver1 + "px";
-    racers[1].style.left = leftDriver2 + "px";
-    racers[2].style.left = leftDriver3 + "px";
-    racers[3].style.left = leftDriver4 + "px";
+    leftRacingCar += getRandomNumber(21);
+    leftMotorbike += getRandomNumber(21);
+    leftPoliceCar += getRandomNumber(21);
+    leftTractor += getRandomNumber(21);
+    racers[0].style.left = leftRacingCar + "px";
+    racers[1].style.left = leftMotorbike + "px";
+    racers[2].style.left = leftPoliceCar + "px";
+    racers[3].style.left = leftTractor + "px";
 });
 
 boostButton.addEventListener("click", function(e) {
     e.stopPropagation();
-    leftDriver1 += 20;
-    racers[0].style.left = leftDriver1 + "px";
+    leftRacingCar += 20;
+    racers[0].style.left = leftRacingCar + "px";
+});
+
+document.addEventListener("keypress", function(evt) {
+    console.log(evt.keyCode);
+    if (evt.keyCode === 32) {
+        board.style.backgroundColor = "rgb(" + getRandomNumber(256) + "," + getRandomNumber(256) + "," + getRandomNumber(256) + ")";
+    }
 });
